0328dfe9fe3e55d4369b8913779df620221a1255
Boudewijn Bout <boudewijn_bout@outlook.com>
2023-12-28 01:35:59 +0100

    fix: Removed redundant route + comment

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,8 +1,5 @@
 export async function load() {
     const results = await fetch('https://jsonplaceholder.typicode.com/posts');
-
-    // Delay the response
-
     const recipes = await results.json();
 
     return {
